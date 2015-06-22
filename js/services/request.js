dc5a13ae396bcfe807b0713d9030128afce6b9b9
mohamedMok <Mohamed@MacBook-Air-de-Mohamed.local>
2015-06-22 13:09:01 +1200

    request on amazon

--- js/services/request.js
+++ js/services/request.js
@@ -1,15 +1,15 @@
  app.factory('User', ['$resource', function($resource) {
 
-	return $resource('http://localhost:3000/users/:id', {id : '@id'}, {
-		all: {method: "GET", isArray: true, url: "http://localhost:3000/users"},
+	return $resource('http://ec2-54-154-80-189.eu-west-1.compute.amazonaws.com/users/:id', {id : '@id'}, {
+		all: {method: "GET", isArray: true, url: "http://ec2-54-154-80-189.eu-west-1.compute.amazonaws.com/users"},
 		byConsent: {method: "GET", isArray: true, url: "http://localhost:3000/consents/people"},
 		logIn: {method:"PUT", url:"http://localhost:3000/users"}
 	});
 }]);
 
  app.factory('Consent', ['$resource', function($resource) {
-	return $resource('http://localhost:3000/consents/:_id', {_id : '@_id'}, {
-		all: {method: "GET", isArray: true, url: "http://localhost:3000/consents"},
-		byUser: {method: "GET", isArray: true, url: "http://localhost:3000/users/:userId/consents"}
+	return $resource('http://ec2-54-154-80-189.eu-west-1.compute.amazonaws.com/consents/:_id', {_id : '@_id'}, {
+		all: {method: "GET", isArray: true, url: "http://ec2-54-154-80-189.eu-west-1.compute.amazonaws.com/consents"},
+		byUser: {method: "GET", isArray: true, url: "http://ec2-54-154-80-189.eu-west-1.compute.amazonaws.com/users/:userId/consents"}
 	});
 }]);
