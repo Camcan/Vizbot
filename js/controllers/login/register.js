dda090ce9bd875e3cee632b99e9f02a3c03739f4
mohamedMok <Mohamed@MacBook-Air-de-Mohamed.local>
2015-08-03 13:52:20 +1200

    Register url

--- js/controllers/login/register.js
+++ js/controllers/login/register.js
@@ -10,7 +10,7 @@ app.controller('RegisterCtrl', ['$scope', '$http', '$location', function ($scope
 				phone : $scope.register.phone,
 				registration : $scope.register.registration	
 		};
-		var res = $http.post("http://localhost:3000/users", user);
+		var res = $http.post("http://ec2-52-18-52-59.eu-west-1.compute.amazonaws.com/users", user);
 		res.success(function(data, status, headers, config) {
 			$scope.message = data;
 			$scope.userId = headers.id;
